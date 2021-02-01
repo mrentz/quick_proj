f428f758506de3949bdbffe0e4061d21f769e89e
Matthew Rentz <matthew.rentz@digitize.company>
2021-02-01 14:00:18 +1030

    styling courses with cards

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,5 @@ Rails.application.routes.draw do
   get 'home/index'
   root "home#index"
   resources :courses
-#  root "static_pages#landing_page"
-  # get 'static_pages/privacy_policy'
   get "privacy_policy", to: "static_pages#privacy_policy"
 end
